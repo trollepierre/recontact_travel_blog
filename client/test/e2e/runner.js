db475fe9e06a802c11f5e4f1dd6cd19150e1542c
Pierre Trollé <ptrolle@octo.com>
2017-08-03 20:10:56 +0200

    lint(all)

--- client/test/e2e/runner.js
+++ client/test/e2e/runner.js
@@ -1,6 +1,6 @@
 // 1. start the dev server using production config
 process.env.NODE_ENV = 'testing';
-var server = require('../../build/dev-server.js');
+const server = require('../../build/dev-server.js');
 
 server.ready.then(() => {
   // 2. run the nightwatch test suite against it
@@ -10,7 +10,7 @@ server.ready.then(() => {
   // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
   // For more information on Nightwatch's config file, see
   // http://nightwatchjs.org/guide#settings-file
-  var opts = process.argv.slice(2);
+  let opts = process.argv.slice(2);
   if (opts.indexOf('--config') === -1) {
     opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
   }
@@ -18,15 +18,15 @@ server.ready.then(() => {
     opts = opts.concat(['--env', 'chrome']);
   }
 
-  var spawn = require('cross-spawn');
-  var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
+  const spawn = require('cross-spawn');
+  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
 
-  runner.on('exit', function (code) {
+  runner.on('exit', (code) => {
     server.close();
     process.exit(code);
   });
 
-  runner.on('error', function (err) {
+  runner.on('error', (err) => {
     server.close();
     throw err;
   });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.json({});
+router.get('/', (req, res, next) => {
+  res.json({});
 });
 
 module.exports = router;
