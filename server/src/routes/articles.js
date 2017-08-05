f83b27a9ee9e4cbe9c2cf298a182ff127a76e47a
Pierre Trollé <ptrolle@octo.com>
2017-08-05 17:38:12 +0200

    bsr(REC-2) : add tests on dropbox service

--- server/src/routes/articles.js
+++ server/src/routes/articles.js
@@ -3,18 +3,16 @@ const DropboxClient = require('../infrastructure/dropbox');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return DropboxClient.getFile()
-    .then((file) => {
-      const article = {
-        imgLink: 'https://www.dropbox.com/s/fghmcj18maztdgv/img0.jpg?dl=1',
-      };
-      const article2 = {
-        imgLink: file,
-      };
-      const articles = [article, article2];
-      return res.json(articles);
-    });
-});
+router.get('/', (req, res) => DropboxClient.getFile()
+  .then((file) => {
+    const article = {
+      imgLink: 'https://www.dropbox.com/s/fghmcj18maztdgv/img0.jpg?dl=1',
+    };
+    const article2 = {
+      imgLink: file,
+    };
+    const articles = [article, article2];
+    return res.json(articles);
+  }));
 
 module.exports = router;
