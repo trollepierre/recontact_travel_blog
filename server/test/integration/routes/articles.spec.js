02a02927b4ca35780af2496f2206680e612814f5
Pierre Trollé <ptrolle@octo.com>
2017-08-06 22:35:47 +0200

    bsr(REC-2) : fix last failing tests

--- server/test/integration/routes/articles.spec.js
+++ server/test/integration/routes/articles.spec.js
@@ -1,12 +1,18 @@
-const { request, expect } = require('../../test-helper');
+const { request, expect, sinon } = require('../../test-helper');
 const app = require('../../../app');
+const DropboxClient = require('../../../src/infrastructure/dropbox');
 
-describe('Integration | Routes | index route', () => {
+describe('Integration | Routes | articles route', () => {
+  afterEach(() => {
+    DropboxClient.getFile.restore();
+  });
+
   it('should have api informations on root', (done) => {
     // Given
     const article = {
       imgLink: 'https://www.dropbox.com/s/fghmcj18maztdgv/img0.jpg?dl=1',
     };
+    sinon.stub(DropboxClient, 'getFile').resolves();
 
     // When
     request(app)
@@ -14,7 +20,7 @@ describe('Integration | Routes | index route', () => {
       .expect('Content-Type', /json/)
       .end((err, response) => {
         // Then
-        expect(response.body).to.deep.equal([article]);
+        expect(response.body).to.deep.equal([article, {}]);
         if (err) {
           done(err);
         }
