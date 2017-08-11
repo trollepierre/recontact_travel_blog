6f16495a4c9f8781b5698eab82d47486388e24bd
Pierre Trollé <ptrolle@octo.com>
2017-08-11 21:04:27 +0200

    feature(REC-2) : display multiple article cards according to response from api

---
+++ client/src/components/ArticleCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="article-card">
+    <article class="article">
+      <h2 class="article__title">{{ article.name }}</h2>
+      <img class="article__image" :src="article.imgLink"/>
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ArticleCard',
+    props: ['article'],
+  };
+</script>
+
+<style scoped>
+  h2 {
+    font-weight: normal;
+  }
+</style>
