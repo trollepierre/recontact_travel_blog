d3b878aa08893312c10eabee9bff5665ef6b4774
Pierre Trollé <ptrolle@octo.com>
2017-08-15 17:24:59 +0200

    bsr(REC-2) : add more test on ArticleCard

--- client/test/unit/specs/components/ArticleCard.spec.js
+++ client/test/unit/specs/components/ArticleCard.spec.js
@@ -7,7 +7,8 @@ describe('ArticleCard.vue', () => {
   beforeEach(() => {
     const article = {
       name: '58 : Pierre avec les webf',
-      imgLink: '../assets/webf.jpg',
+      imgLink: 'webf', // fix provisoire pour le offline
+      // imgLink: '../assets/webf.jpg',
     };
     const Constructor = Vue.extend(ArticleCard);
     component = new Constructor({
@@ -23,7 +24,7 @@ describe('ArticleCard.vue', () => {
 
   describe('$data', () => {
     it('should have isClicked property set to false', () => {
-      expect(component.$data.isClicked).to.equal(false);
+      expect(component.$data.isClicked).to.be.false;
     });
   });
 
@@ -32,11 +33,32 @@ describe('ArticleCard.vue', () => {
       const articleTitle = component.$el.querySelector('.article__title');
       expect(articleTitle.textContent).to.equal('58 : Pierre avec les webf');
     });
+
+    it('should render article image', () => {
+      const articleLink = component.$el.querySelector('img');
+      expect(articleLink.getAttribute('src')).to.contain('webf'); // fix pour le offline
+    });
+
+    it('should render dropbox gallery link', () => {
+      const dropboxLink = component.$el.querySelector('a.article__dropbox');
+      expect(dropboxLink.getAttribute('href')).to.equal('http://dropbox.com');
+      expect(dropboxLink.getAttribute('target')).to.equal('_blank');
+    });
+
+    it('should have enabled button', () => {
+      expect(component.$el.querySelector('.article__view-button').disabled).to.be.false;
+    });
   });
 
-  describe('on click on button "voir les photos"', () => {
-    it('should disable button');
+  describe('clicking on button "Voir les photos"', () => {
+    it('should disable button', () => {
+      // when
+      component.$el.querySelector('button.article__view-button').click();
 
-    it('should open target blank to dropbox');
+      // then
+      Vue.nextTick().then(() => {
+        expect(component.$el.querySelector('.article__view-button').disabled).to.be.true;
+      });
+    });
   });
 });
