1739b344b381bc611265d9b7073ee9eecbd583b0
Pierre Trollé <ptrolle@octo.com>
2017-08-17 14:02:40 +0200

    revert(revert(5bddf48))

--- client/build/dev-server.js
+++ client/build/dev-server.js
@@ -1,93 +1,91 @@
-require('./check-versions')();
+require('./check-versions')()
 
-const config = require('../config/index');
-
+var config = require('../config')
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-const opn = require('opn');
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const proxyMiddleware = require('http-proxy-middleware');
-const webpackConfig = process.env.NODE_ENV === 'testing'
+var opn = require('opn')
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
+var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf');
+  : require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
-const port = process.env.PORT || config.dev.port;
+var port = process.env.PORT || config.dev.port
 // automatically open browser, if not set will be false
-const autoOpenBrowser = !!config.dev.autoOpenBrowser;
+var autoOpenBrowser = !!config.dev.autoOpenBrowser
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-const proxyTable = config.dev.proxyTable;
+var proxyTable = config.dev.proxyTable
 
-const app = express();
-const compiler = webpack(webpackConfig);
+var app = express()
+var compiler = webpack(webpackConfig)
 
-const devMiddleware = require('webpack-dev-middleware')(compiler, {
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
-  quiet: true,
-});
+  quiet: true
+})
 
-const hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {},
-  heartbeat: 2000,
-});
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+})
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', (compilation) => {
-  compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
-    hotMiddleware.publish({ action: 'reload' });
-    cb();
-  });
-});
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 // proxy api requests
-Object.keys(proxyTable).forEach((context) => {
-  let options = proxyTable[context];
+Object.keys(proxyTable).forEach(function (context) {
+  var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options };
+    options = { target: options }
   }
-  app.use(proxyMiddleware(options.filter || context, options));
-});
+  app.use(proxyMiddleware(options.filter || context, options))
+})
 
 // handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')());
+app.use(require('connect-history-api-fallback')())
 
 // serve webpack bundle output
-app.use(devMiddleware);
+app.use(devMiddleware)
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware);
+app.use(hotMiddleware)
 
 // serve pure static assets
-const staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
-app.use(staticPath, express.static('./static'));
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+app.use(staticPath, express.static('./static'))
 
-const uri = `http://localhost:${port}`;
+var uri = 'http://localhost:' + port
 
-let _resolve;
-const readyPromise = new Promise((resolve) => {
-  _resolve = resolve;
-});
+var _resolve
+var readyPromise = new Promise(resolve => {
+  _resolve = resolve
+})
 
-console.log('> Starting dev server...');
+console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
-  console.log(`> Listening at ${uri}\n`);
+  console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri);
+    opn(uri)
   }
-  _resolve();
-});
+  _resolve()
+})
 
-const server = app.listen(port);
+var server = app.listen(port)
 
 module.exports = {
   ready: readyPromise,
   close: () => {
-    server.close();
-  },
-};
+    server.close()
+  }
+}
--- client/build/utils.js
+++ client/build/utils.js
@@ -1,35 +1,35 @@
-const path = require('path');
-const config = require('../config/index');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+var config = require('../config')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 exports.assetsPath = function (_path) {
-  const assetsSubDirectory = process.env.NODE_ENV === 'production'
+  var assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory;
-  return path.posix.join(assetsSubDirectory, _path);
-};
+    : config.dev.assetsSubDirectory
+  return path.posix.join(assetsSubDirectory, _path)
+}
 
 exports.cssLoaders = function (options) {
-  options = options || {};
+  options = options || {}
 
-  const cssLoader = {
+  var cssLoader = {
     loader: 'css-loader',
     options: {
       minimize: process.env.NODE_ENV === 'production',
-      sourceMap: options.sourceMap,
-    },
-  };
+      sourceMap: options.sourceMap
+    }
+  }
 
   // generate loader string to be used with extract text plugin
-  function generateLoaders(loader, loaderOptions) {
-    const loaders = [cssLoader];
+  function generateLoaders (loader, loaderOptions) {
+    var loaders = [cssLoader]
     if (loader) {
       loaders.push({
-        loader: `${loader}-loader`,
+        loader: loader + '-loader',
         options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap,
-        }),
-      });
+          sourceMap: options.sourceMap
+        })
+      })
     }
 
     // Extract CSS when that option is specified
@@ -37,10 +37,11 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader',
-      });
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
     }
-    return ['vue-style-loader'].concat(loaders);
   }
 
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
@@ -51,20 +52,20 @@ exports.cssLoaders = function (options) {
     sass: generateLoaders('sass', { indentedSyntax: true }),
     scss: generateLoaders('sass'),
     stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus'),
-  };
-};
+    styl: generateLoaders('stylus')
+  }
+}
 
 // Generate loaders for standalone style files (outside of .vue)
 exports.styleLoaders = function (options) {
-  const output = [];
-  const loaders = exports.cssLoaders(options);
-  for (const extension in loaders) {
-    const loader = loaders[extension];
+  var output = []
+  var loaders = exports.cssLoaders(options)
+  for (var extension in loaders) {
+    var loader = loaders[extension]
     output.push({
-      test: new RegExp(`\\.${extension}$`),
-      use: loader,
-    });
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
   }
-  return output;
-};
+  return output
+}
