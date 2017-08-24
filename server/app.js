4b9d3839f5b67c29812f78451e1dc4938d188cae
Pierre Trollé <ptrolle@octo.com>
2017-08-24 23:16:07 +0200

    fix(dev) : remove server side rendering

--- server/app.js
+++ server/app.js
@@ -10,10 +10,6 @@ const users = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
