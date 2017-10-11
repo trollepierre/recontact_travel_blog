752b73365b89422eeaa927226f681bae328bc447
Pierre Trollé <ptrolle@octo.com>
2017-10-11 22:46:09 +0200

    bsr(REC-14) : remove console log

--- server/src/domain/services/chapter-service.js
+++ server/src/domain/services/chapter-service.js
@@ -1,9 +1,6 @@
 const { Chapter } = require('../models');
 
 function createChapters(chapters) {
-  console.log('E. chapters');
-  console.log(chapters);
-
   return Chapter
     .bulkCreate(chapters.chapters);
 }
