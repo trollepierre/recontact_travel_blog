6e13cc310784b48f11bab7c331dbd3120f1b4310
Pierre Trollé <ptrolle@octo.com>
2017-10-29 10:06:21 +0100

    bsr(REC-14) : remove quick articles

--- server/src/routes/quickArticles.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const articles = require('../../test/unit/fixtures/articlesWithSharedLink');
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.json(articles));
-
-module.exports = router;
