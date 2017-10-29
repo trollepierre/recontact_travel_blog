f7fd4695e155d118c997655eaf7b4171bd927058
Pierre Trollé <ptrolle@octo.com>
2017-10-29 10:00:07 +0100

    review(REC-14) : remove comment

--- server/src/use_cases/get-all-articles.js
+++ server/src/use_cases/get-all-articles.js
@@ -1,6 +1,5 @@
 const articleRepository = require('../domain/repositories/article-repository');
 
-// TODO what if database is not synchronized?
 function getAllArticles() {
   return articleRepository.getAll();
 }
