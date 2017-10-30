5936c36f21647a2bb193b7d28c4f68070f80e1b8
Pierre Trollé <ptrolle@octo.com>
2017-10-31 00:32:20 +0100

    bsr(dev) : display console log when dropbox throws an error

--- server/src/infrastructure/external_services/dropbox-client.js
+++ server/src/infrastructure/external_services/dropbox-client.js
@@ -7,17 +7,29 @@ const DropboxClient = {
 
   getAllDropboxFoldersMetadatas() {
     return DropboxApi.filesListFolder({ path: '', recursive: true })
-      .then(response => response.entries.filter(metadata => metadata['.tag'] === 'folder'));
+      .then(response => response.entries.filter(metadata => metadata['.tag'] === 'folder'))
+      .catch((err) => {
+        console.log('Erreur lors de la récupération de tous les fichiers Dropbox : ');
+        console.log(err);
+      });
   },
 
   getTextFileStream(id) {
     return DropboxApi.filesGetTemporaryLink({ path: `/${id}/fr.php` })
-      .then(result => result.link);
+      .then(result => result.link)
+      .catch((err) => {
+        console.log('Erreur lors de la récupération du fichier texte de : ', `/${id}/fr.php`);
+        console.log(err);
+      });
   },
 
   createSharedLink(path) {
     const options = { path, short_url: false };
-    return DropboxApi.sharingCreateSharedLink(options);
+    return DropboxApi.sharingCreateSharedLink(options)
+      .catch((err) => {
+        console.log('Erreur lors de la création du lien de : ', path);
+        console.log(err);
+      });
   },
 
 };
