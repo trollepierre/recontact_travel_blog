df68d2c58be8975204f0b919179c95c29c94320c
Pierre Trollé <ptrolle@octo.com>
2017-11-01 20:26:27 +0100

    feature(REC-18) : add notif about sync

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Toastr from 'vue-toastr';
 import App from './App';
 import router from './router/index';
+
+Vue.component('vue-toastr', Toastr);
+require('vue-toastr/dist/vue-toastr.css');
 
 Vue.config.productionTip = false;
 
@@ -10,6 +14,9 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
+  template: '<div><App/><vue-toastr ref="toastr"></vue-toastr></div>',
   components: { App },
+  mounted() {
+    this.$refs.toastr.defaultPosition = 'toast-bottom-right';
+  },
 });
