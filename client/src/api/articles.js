af42b2fb4e1dfc5fc9d4004b1a87b09644c40831
Pierre Trollé <ptrolle@octo.com>
2017-11-02 21:32:51 +0100

    bsr(REC-19) : remove console log

--- client/src/api/articles.js
+++ client/src/api/articles.js
@@ -4,21 +4,10 @@ const ArticlesApi = {
 
   fetchAll() {
     const url = `${process.env.API_URL}api/articles`;
-    console.log('url : ', url);
-
     const options = { headers: { 'Content-Type': 'application/json' } };
 
     return axios.get(url, options)
-      .then((response) => {
-        console.log('rapide');
-        console.log(response.data);
-        return response.data;
-      })
-      .catch((error) => {
-        console.log('trop lent');
-
-        return Promise.reject(error);
-      });
+      .then(response => response.data);
   },
 };
 
