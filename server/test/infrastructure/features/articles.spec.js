110f45fce861f17cae6b43fd32c9d66cdde31790
Pierre Trollé <ptrolle@octo.com>
2017-11-11 14:43:07 +0100

    feature(REC-47) : get photos is ready

--- server/test/infrastructure/features/articles.spec.js
+++ server/test/infrastructure/features/articles.spec.js
@@ -1,9 +1,11 @@
 const { request, expect, sinon } = require('../../test-helper');
 const app = require('../../../app');
+const GetPhotosOfArticle = require('../../../src/use_cases/get-photos-of-article');
 const GetAllArticles = require('../../../src/use_cases/get-all-articles');
 const GetArticle = require('../../../src/use_cases/get-article');
 const article = require('../../fixtures/articleSaved');
 const chapter = require('../../fixtures/chapterWithParagraphs');
+const photo = require('../../fixtures/photo');
 
 describe('Integration | Routes | articles route', () => {
   describe('/articles', () => {
@@ -45,7 +47,7 @@ describe('Integration | Routes | articles route', () => {
       GetArticle.getAllChapters.restore();
     });
 
-    it('should call ChapterRepository to getChaptersOfArticle, serialize it before sending json', (done) => {
+    it('should call ChapterRepository to getChaptersOfArticle before sending json', (done) => {
       // Given
       const stringIdArticle = '59';
 
@@ -57,6 +59,36 @@ describe('Integration | Routes | articles route', () => {
           // Then
           expect(GetArticle.getAllChapters).to.have.been.calledWith(stringIdArticle);
           expect(response.body).to.deep.equal(chapters);
+          if (err) {
+            done(err);
+          }
+          done();
+        });
+    });
+  });
+
+  describe('/articles/:id/photos', () => {
+    const photos = [photo(), photo()];
+    beforeEach(() => {
+      sinon.stub(GetPhotosOfArticle, 'getAllPhotos').resolves(photos);
+    });
+
+    afterEach(() => {
+      GetPhotosOfArticle.getAllPhotos.restore();
+    });
+
+    it('should call GetPhotosOfArticle to getAllPhotos before sending json', (done) => {
+      // Given
+      const stringIdArticle = '59';
+
+      // When
+      request(app)
+        .get(`/api/articles/${stringIdArticle}/photos`)
+        .expect('Content-Type', /json/)
+        .end((err, response) => {
+          // Then
+          expect(GetPhotosOfArticle.getAllPhotos).to.have.been.calledWith(stringIdArticle);
+          expect(response.body).to.deep.equal(photos);
           if (err) {
             done(err);
           }
