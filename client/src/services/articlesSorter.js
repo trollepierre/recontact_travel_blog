fc13adc3258b99a15a75d19fb2199598033daeac
Pierre Trollé <ptrolle@octo.com>
2017-11-20 19:13:21 +0100

    review(REC-38) : extract private function outside export

--- client/src/services/articlesSorter.js
+++ client/src/services/articlesSorter.js
@@ -1,19 +1,17 @@
-export default {
+function getDropboxId(article) {
+  return parseInt(article.dropboxId, 10);
+}
 
+export default {
   sortByDropboxId(articles) {
     return articles.sort((articleA, articleB) => {
-      if (this.getDropboxId(articleA) < this.getDropboxId(articleB)) {
+      if (getDropboxId(articleA) < getDropboxId(articleB)) {
         return 1;
       }
-      if (this.getDropboxId(articleA) > this.getDropboxId(articleB)) {
+      if (getDropboxId(articleA) > getDropboxId(articleB)) {
         return -1;
       }
       return 0;
     });
   },
-
-  getDropboxId(article) {
-    return parseInt(article.dropboxId, 10);
-  },
-
 };
