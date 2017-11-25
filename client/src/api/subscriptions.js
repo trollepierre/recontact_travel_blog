07489eab0c3e44d43720fa8c8679ee5f94c7a494
Pierre Trollé <ptrolle@octo.com>
2017-11-25 09:03:25 +0100

    feature(REC-70) : add language in subscription form

--- client/src/api/subscriptions.js
+++ client/src/api/subscriptions.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import translationsService from '@/services/translations';
 
 export default {
 
   subscribe(email) {
+    const lang = translationsService.getNavigatorLanguage();
     const url = `${process.env.API_URL}api/subscriptions`;
     const options = { headers: { 'Content-Type': 'application/json' } };
-    return axios.post(url, { email }, options);
+    return axios.post(url, { email, lang }, options);
   },
 };
