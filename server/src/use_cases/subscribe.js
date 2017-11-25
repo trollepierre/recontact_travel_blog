312af22b06a9e8ee1f78dcef641b434ecbfdb3c7
Pierre Trollé <ptrolle@octo.com>
2017-11-25 08:56:33 +0100

    feature(REC-70) : save language in database if set

--- server/src/use_cases/subscribe.js
+++ server/src/use_cases/subscribe.js
@@ -1,11 +1,11 @@
 const subscriptionRepository = require('../domain/repositories/subscription-repository');
 const { isEmpty } = require('lodash');
 
-function subscribe(userEmail) {
-  return _findSubscription(userEmail)
+function subscribe(subscriptionForm) {
+  return _findSubscription(subscriptionForm.email)
     .then(subscription => (!isEmpty(subscription) ?
       { subscription, created: false } :
-      _createSubscription(userEmail)
+      _createSubscription(subscriptionForm)
         .then(createdSubscription => ({ subscription: createdSubscription, created: true }))));
 }
 
@@ -13,8 +13,8 @@ function _findSubscription(userEmail) {
   return subscriptionRepository.getByEmail(userEmail);
 }
 
-function _createSubscription(userEmail) {
-  return subscriptionRepository.create(userEmail);
+function _createSubscription(subscription) {
+  return subscriptionRepository.create(subscription);
 }
 
 module.exports = {
