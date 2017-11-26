c457bef7b427a9e17ffd2538c9d7faaeecef2667
Pierre Trollé <ptrolle@octo.com>
2017-11-26 01:03:05 +0100

    wip

--- client/src/components/ArticlePage.vue
+++ client/src/components/ArticlePage.vue
@@ -10,14 +10,22 @@
             </li>
           </ul>
         </section>
-        <aside>
-          <h2 class="article-page__photo-title">{{ $t("hereTheGallery") }}</h2>
-          <ul class="photo__list">
+        <aside class="article-page__photo-gallery photo-gallery">
+          <h2 class="photo-gallery__title">{{ $t("hereTheGallery") }}</h2>
+          <ul class="photo-gallery__list">
             <li v-for="photo in photos" class="photo__item">
               <photo-card :photo="photo"></photo-card>
             </li>
           </ul>
         </aside>
+        <footer class="article-page__footer footer-article">
+          <button class="footer-article__previous" @click="viewPreviousArticle">
+            {{ $t("goToPreviousArticle") }}
+          </button>
+          <button class="footer-article__next" @click="viewNextArticle">
+            {{ $t("goToNextArticle") }}
+          </button>
+        </footer>
       </div>
     </main>
   </div>
@@ -41,6 +49,7 @@
         chapters: [],
         photos: [],
         title: '',
+        dropboxId: parseInt(this.$route.params.id, 10),
       };
     },
     mounted() {
@@ -49,7 +58,7 @@
     },
     methods: {
       getChapters() {
-        chaptersApi.fetch(this.$route.params.id)
+        chaptersApi.fetch(this.dropboxId)
           .then((article) => {
             (this.chapters = article.chapters);
             (this.title = translationsService.getTitle(article));
@@ -61,14 +70,32 @@
             (this.photos = photos);
           });
       },
+      viewPreviousArticle() {
+        this.goToArticle(this.dropboxId - 1);
+      },
+      viewNextArticle() {
+        this.goToArticle(this.dropboxId + 1);
+      },
+      goToArticle(idArticle) {
+        console.log('go to');
+
+        this.$router.push(`/articles/${idArticle}`);
+        this.dropboxId = idArticle;
+        this.getChapters();
+        this.getPhotos();
+      },
     },
     i18n: {
       messages: {
         fr: {
           hereTheGallery: 'Voici la galerie photo de cet article !',
+          goToPreviousArticle: 'Voir l‘article précédent',
+          goToNextArticle: 'Voir l‘article suivant',
         },
         en: {
           hereTheGallery: 'Here is the photo gallery of this article',
+          goToPreviousArticle: 'Read the previous article',
+          goToNextArticle: 'Read the next article',
         },
       },
     },
@@ -97,7 +124,7 @@
     max-width: 80%;
   }
 
-  .chapter__list, .photo__list  {
+  .chapter__list, .photo-gallery__list  {
     width: 100%;
     padding: 0;
     display: flex;
@@ -112,7 +139,7 @@
   }
 
   @media only screen and (min-width: 640px) {
-    .chapter__list, .photo__list  {
+    .chapter__list, .photo-gallery__list  {
       flex-direction: row;
       flex-wrap: wrap;
     }
