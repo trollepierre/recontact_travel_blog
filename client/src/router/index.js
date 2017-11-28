04dfc9646e12159557b33c62584e5de2ea4a0b71
Pierre Trollé <ptrolle@octo.com>
2017-11-28 10:09:38 +0100

    feature(REC-108) : add subscriptions page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import ArticleList from '@/components/ArticleList';
 import ArticlePage from '@/components/ArticlePage';
+import SubscriberList from '@/components/SubscriberList';
 
 Vue.use(Router);
 
@@ -22,6 +23,11 @@ export default new Router({
       name: 'ArticleList',
       component: ArticleList,
       props: { adminMode: true },
+    },
+    {
+      path: '/subscriptions',
+      name: 'SubscriberList',
+      component: SubscriberList,
     },
   ],
 });
