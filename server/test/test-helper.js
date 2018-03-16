18ab9f647688c32a194892dea73afa2d91a85c9a
Pierre Trollé <ptrolle@octo.com>
2018-03-17 00:48:05 +0100

    bsr(REC-127) : remove comments

--- server/test/test-helper.js
+++ server/test/test-helper.js
@@ -1,23 +1,15 @@
-// Mocha
 const {
   describe, it, before, after, beforeEach, afterEach,
 } = require('mocha');
 
-// Sinon
 const sinon = require('sinon');
-
 const { assert } = sinon;
 
-// Chai
 const chai = require('chai');
-
 const { expect } = chai;
-
-// Chai plugins
 chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
 
-// Supertest
 const request = require('supertest');
 
 module.exports = {
