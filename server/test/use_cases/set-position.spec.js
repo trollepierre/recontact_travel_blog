3212deaa5b2ccedfa3de7fdb3a24a2c2c0f851c2
Pierre Trollé <ptrolle@octo.com>
2018-06-08 20:31:19 +0200

    tech : add missing tests on set position

---
+++ server/test/use_cases/set-position.spec.js
@@ -0,0 +1,35 @@
+const { expect, sinon } = require('../test-helper');
+const SetPosition = require('../../src/use_cases/set-position');
+const PositionRepository = require('../../src/domain/repositories/position-repository');
+
+describe('Unit | SetPosition | setPosition', () => {
+  const position = { lastPosition: 'Mexico' }
+  const persistedPosition = { id: 1, lastPosition: 'Mexico' };
+
+  beforeEach(() => {
+    sinon.stub(PositionRepository, 'create').resolves(persistedPosition);
+  });
+
+  afterEach(() => {
+    PositionRepository.create.restore();
+  });
+
+  it('should call PositionRepository to create articles', () => {
+    // when
+    SetPosition.setPosition(position);
+
+    // then
+    expect(PositionRepository.create).to.have.been.calledWith(position);
+  });
+
+  it('should return positions', () => {
+    // when
+    const promise = SetPosition.setPosition(position);
+
+    // then
+    return promise.then((returnedPositions) => {
+      expect(returnedPositions).to.deep.equal(persistedPosition);
+    });
+  });
+});
+
