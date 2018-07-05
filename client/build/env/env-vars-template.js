feabe5a4eafa6dfe6b2dd539e6d4a8d1e8ba81d5
Pierre Trollé <ptrolle@octo.com>
2018-07-05 23:14:27 +0200

    chore : fix webpack 4 build

---
+++ client/build/env/env-vars-template.js
@@ -0,0 +1,7 @@
+// This file will not end up inside the main application JavaScript bundle.
+// Instead, it will simply be copied inside the build folder.
+// The generated "index.html" will require it just before this main bundle.
+// You can thus use it to define some environment variables that will
+// be made available synchronously in all your JS modules under "src".
+
+window.env = {}
