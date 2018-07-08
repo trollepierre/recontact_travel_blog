92113fb1784707a6d6728f19bc7260383015b345
Pierre Trollé <ptrolle@octo.com>
2018-07-08 11:50:56 +0200

    bsr : remove warning in App.spec

--- client/src/App.spec.js
+++ client/src/App.spec.js
@@ -1,9 +1,11 @@
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 describe('Component | App', () => {
   let localVue
   beforeEach(() => {
     localVue = createLocalVue()
+    localVue.use(VueRouter)
   })
 
   describe('template', () => {
