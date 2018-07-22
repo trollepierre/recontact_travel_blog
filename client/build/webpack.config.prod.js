a00ade01ad68eaa39bf52f05a64c066f5e50a4e1
Pierre Trollé <ptrolle@octo.com>
2018-07-22 15:51:38 +0200

    bsr : clean config

--- client/build/webpack.config.prod.js
+++ client/build/webpack.config.prod.js
@@ -93,14 +93,6 @@ module.exports = {
   },
 
   plugins: [
-    /*new BrotliGzipPlugin({
-      asset: '[path].br[query]',
-      algorithm: 'brotli',
-      test: /\.(js|css|html|svg)$/,
-      threshold: 10240,
-      minRatio: 0.8,
-      quality: 11
-    }),*/
     new BrotliGzipPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
