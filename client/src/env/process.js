abc19bf378fa006603e7182706a3a7128e54fcb6
Pierre Trollé <ptrolle@octo.com>
2018-07-22 17:28:50 +0200

    fix : typo

--- client/src/env/process.js
+++ client/src/env/process.js
@@ -1,8 +1,8 @@
 const PRODUCTION_MODE = 'production'
 const DEVELOPMENT_MODE = 'development'
 
-isProduction = () => process.env.NODE_ENV === PRODUCTION_MODE
-isDevelopment = () => process.env.NODE_ENV === DEVELOPMENT_MODE
+const isProduction = () => process.env.NODE_ENV === PRODUCTION_MODE
+const isDevelopment = () => process.env.NODE_ENV === DEVELOPMENT_MODE
 
 export {
   isProduction,
