873328c3362da46f8cfadee00aceab899837136f
Pierre Trollé <ptrolle@octo.com>
2018-07-22 15:43:09 +0200

    chore : uniformise eslintrc files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    root: true,
-    extends: 'airbnb-base',
-    rules: {
-        'no-underscore-dangle': 0,
-        'no-tabs': 2,
-        'padded-blocks': [2, {
-            'blocks': 'never',
-            'classes': 'always',
-            'switches': 'never'
-        }],
-        'no-console': 0,
-        'max-len': 0,
-    }
+  root: true,
+  extends: 'airbnb-base',
+  rules: {
+    'no-underscore-dangle': 0,
+    'no-tabs': 2,
+    'padded-blocks': [2, {
+      blocks: 'never',
+      classes: 'always',
+      switches: 'never',
+    }],
+    'no-console': 0,
+    'max-len': 0,
+  },
 };
