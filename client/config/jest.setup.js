5365c89d1f4001df4a12e80db0bc4f87af300bbe
Pierre Trollé <ptrolle@octo.com>
2018-07-22 15:51:38 +0200

    bsr : clean config

--- client/config/jest.setup.js
+++ client/config/jest.setup.js
@@ -1,12 +1,5 @@
 import { createLocalVue, shallowMount, mount, RouterLinkStub } from '@vue/test-utils'
 
-// Great workaround to remove dumb Vuetify warnings when running tests
-// https://github.com/vuetifyjs/vuetify/issues/1210
-// https://cdn.24.co.za/files/Cms/General/d/3996/0be99e80265c4ce986fbfcf627a3a82e.gif
-const app = document.createElement('div')
-app.setAttribute('data-app', true)
-document.body.appendChild(app)
-
 // Export vue-test-utils methods globally
 global.RouterLinkStub = RouterLinkStub
 global.createLocalVue = createLocalVue
