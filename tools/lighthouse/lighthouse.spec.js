5d78fc6e73194af97d09607bf216792188f27d50
Pierre Trollé <ptrolle@octo.com>
2018-10-05 21:43:20 +0200

    tech: clean lighthouse spec code

--- tools/lighthouse/lighthouse.spec.js
+++ tools/lighthouse/lighthouse.spec.js
@@ -1,41 +1,44 @@
-const lighthouse = require('lighthouse') // Node CLI for Lighthouse https://www.npmjs.com/package/lighthouse#using-the-node-cli
-const chromeLauncher = require('chrome-launcher') // Launch Chrome from node
+// Inspired from https://github.com/zgreen/lighthouse-circleci-example/blob/master/lighthouse.test.js
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
 
-jest.setTimeout(60000)
+const TIME_FOR_LIGHTHOUSE_FULL_TEST = 60000;
+jest.setTimeout(TIME_FOR_LIGHTHOUSE_FULL_TEST);
 
-const launchChromeAndRunLighthouse = (
-  url,
-  opts = { chromeFlags: [] },
-  config = null,
-) =>
-  chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then((chrome) => {
-    opts.port = chrome.port
-    return lighthouse(url, opts, config).then(results =>
-      chrome.kill().then(() => {
-        // console.log(results);
+const launchChromeAndRunLighthouse = async (url) => {
+  const chrome = await chromeLauncher.launch({ chromeFlags: [] });
+  const opts = { chromeFlags: [], port: chrome.port };
+  const configJSON = null;
+  const results = await lighthouse(url, opts, configJSON);
+  await chrome.kill();
+  return results;
+};
 
-        return results
-      }))
-  })
+const buildLighthouseReport = (results) => {
+  const { categories } = results.lhr;
+  let rows = '';
+  const scores = Object.values(categories).map(({ score }) => score * 100);
+  const lighthouseScore = scores.reduce((a, b) => a + b, 0) / scores.length;
+
+  Object.values(categories).forEach((cat) => {
+    rows += `| ${cat.title} | ${cat.score * 100} |\n`;
+  });
+  console.log('Total Lighthouse score is : ', lighthouseScore);
+  console.log(rows);
+
+  return { lighthouseScore, categories };
+};
 
 test('Meaningful first paint score', () =>
-  launchChromeAndRunLighthouse('https://recontact-branch.herokuapp.com//').then((results) => {
-    const { categories } = results.lhr
-    let rows = ''
-    const scores = Object.values(categories).map(({ score }) => score * 100)
-    const calcul = scores.reduce((a, b) => a + b, 0) / scores.length
+  launchChromeAndRunLighthouse('https://recontact-branch.herokuapp.com/')
+    .then(buildLighthouseReport)
+    .then((report) => {
+      const { categories, lighthouseScore } = report;
 
-    Object.values(categories).forEach((cat) => {
-      rows += `| ${cat.title} | ${cat.score * 100} |\n`
-    })
-
-    console.log('Total score is : ', calcul)
-    console.log(rows)
-
-    expect(calcul).toBeGreaterThanOrEqual(65) // 68.6
-    expect(categories.performance.score * 100).toBeGreaterThanOrEqual(30) // 45, 55, 43
-    expect(categories.pwa.score * 100).toBeGreaterThanOrEqual(45) // 46
-    expect(categories.accessibility.score * 100).toBeGreaterThanOrEqual(70) // 73
-    expect(categories['best-practices'].score * 100).toBeGreaterThanOrEqual(75) // 80
-    expect(categories.seo.score * 100).toBeGreaterThanOrEqual(85) // 89
-  }))
+      expect(lighthouseScore).toBeGreaterThanOrEqual(65); // 68.6
+      expect(categories.performance.score * 100).toBeGreaterThanOrEqual(30); // 45, 55, 43, 40
+      expect(categories.pwa.score * 100).toBeGreaterThanOrEqual(46); // 46
+      expect(categories.accessibility.score * 100).toBeGreaterThanOrEqual(73); // 73
+      expect(categories['best-practices'].score * 100).toBeGreaterThanOrEqual(80); // 80
+      expect(categories.seo.score * 100).toBeGreaterThanOrEqual(89); // 89
+    }));
