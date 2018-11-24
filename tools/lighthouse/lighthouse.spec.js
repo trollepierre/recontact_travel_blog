34a7e3781a20d7567dee9db99c5b9387569f64fd
Pierre Trollé <ptrolle@octo.com>
2018-11-24 10:53:09 +0100

    tech(REC-188): adjust seo score since canonical refers to another url

--- tools/lighthouse/lighthouse.spec.js
+++ tools/lighthouse/lighthouse.spec.js
@@ -40,5 +40,5 @@ test('Meaningful first paint score', () =>
       expect(categories.pwa.score * 100).toBeGreaterThanOrEqual(46); // 46
       expect(categories.accessibility.score * 100).toBeGreaterThanOrEqual(73); // 73
       expect(categories['best-practices'].score * 100).toBeGreaterThanOrEqual(80); // 80
-      expect(categories.seo.score * 100).toBeGreaterThanOrEqual(89); // 89, 90
+      expect(categories.seo.score * 100).toBeGreaterThanOrEqual(82); // 82
     }));
