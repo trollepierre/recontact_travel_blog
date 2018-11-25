ac076bcc8256d116b2d5bcdc32b42b87f25fad70
Pierre Trollé <ptrolle@octo.com>
2018-11-25 11:31:12 +0100

    chore(REC-194): fix yarn start - db-config should be in js

--- server/src/domain/models/index.js
+++ server/src/domain/models/index.js
@@ -1,23 +1,21 @@
 import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
+import { dbConfig } from '../../infrastructure/db/db-config'
+import env from '../../infrastructure/env/env'
+import { isProduction } from '../../infrastructure/env/process'
 
-require('dotenv').config()
-
-const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || 'development'
-const config = require('../../infrastructure/db/config.json')[env]
-
-const db = {}
+const config = dbConfig()
 
 let sequelize
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable])
+if (isProduction()) {
+  sequelize = new Sequelize(env('DATABASE_URL'))
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_NAME, config.username, config.password, config)
+  sequelize = new Sequelize(env('DATABASE_NAME'), config.username, config.password, config)
 }
 
+const basename = path.basename(__filename)
+const db = {}
 fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
--- server/src/infrastructure/env/env.js
+++ server/src/infrastructure/env/env.js
@@ -1,10 +1,10 @@
 import { load } from 'dotenv-extended'
-import { env } from './process'
+import { envKey } from './process'
 
 const dotenvVars = load()
 
 export default key => {
-  const processEnv = env(key)
+  const processEnv = envKey(key)
   return processEnv !== undefined
     ? processEnv
     : dotenvVars[key]
