e981cf0734ac818ce96c92b315f9878dc06e79ae
Pierre TROLLE <pierre.trolle@manomano.com>
2019-05-17 20:22:22 +0200

    fix new tests

--- server/test/use_cases/get-article-comments.spec.js
+++ server/test/use_cases/get-article-comments.spec.js
@@ -2,26 +2,26 @@ import { expect, sinon } from '../test-helper'
 import GetArticleComments from '../../src/use_cases/get-article-comments'
 import CommentRepository from '../../src/domain/repositories/comment-repository'
 
-describe.skip('Unit | GetArticleComments | getArticleComments', () => {
+describe('Unit | GetArticleComments | getArticleComments', () => {
   const comments = [
     { id: 1, text: 'comment-1' },
     { id: 2, text: 'comment-2' },
   ]
 
   beforeEach(() => {
-    sinon.stub(CommentRepository, 'getAll').resolves(comments)
+    sinon.stub(CommentRepository, 'getAllById').resolves(comments)
   })
 
   afterEach(() => {
-    CommentRepository.getAll.restore()
+    CommentRepository.getAllById.restore()
   })
 
-  it('should call CommentRepository to getAll comments', () => {
+  it('should call CommentRepository to getAllById comments', () => {
     // when
     GetArticleComments.getArticleComments()
 
     // then
-    expect(CommentRepository.getAll).to.have.been.calledWith()
+    expect(CommentRepository.getAllById).to.have.been.calledWith()
   })
 
   it('should return all comments', () => {
