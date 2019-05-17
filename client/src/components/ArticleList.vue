91cf38fe6896138e4f73ab69ef0dc7dd9d5b4035
Pierre TROLLE <pierre.trolle@manomano.com>
2019-05-17 18:26:32 +0200

    extract position form inside a component

--- client/src/components/ArticleList.vue
+++ client/src/components/ArticleList.vue
@@ -17,74 +17,14 @@
             </span>
           </p>
           <template v-if="adminMode">
-            <a href="http://recontact.me/apo/sub">
-              <button
-                class="article-results__buttons article-results__sync_hidden"
-                type="button"
-                @click.prevent="goToSubscriptions">
-                {{ $t("getSubscribers") }}
-              </button>
-            </a>
-            <button
-              :disabled="isClickedSync"
-              class="article-results__buttons"
-              type="button"
-              @click.prevent="synchronise">
-              {{ $t("getNewArticles") }}
-            </button>
-            <button
-              :disabled="isClickedSync"
-              class="article-results__buttons article-results__sync_hidden"
-              type="button"
-              @click.prevent="deleteAll">
-              {{ $t("deleteAllArticles") }}
-            </button>
-            <button
-              :disabled="isClickedSync"
-              class="article-results__buttons article-results__sync_hidden"
-              type="button"
-              @click.prevent="deleteAndSyncAll">
-              {{ $t("deleteAndSyncAllArticles") }}
-            </button>
-            <br>
-
-            <form class="article-results__form">
-              <p class="article-results__text">
-                {{ $t("lastPosition") }}
-              </p>
-              <label
-                class="article-results__label"
-                for="position-place">
-                {{ $t("place") }}
-              </label>
-              <input
-                id="position-place"
-                v-model="place"
-                class="article-results__input article-results__place"
-                placeholder="Paris">
-              <label
-                class="article-results__label"
-                for="position-time">
-                {{ $t("time") }}
-              </label>
-              <input
-                id="position-time"
-                v-model="time"
-                class="article-results__input article-results__time"
-                placeholder="le 1er mai 2018">
-              <button
-                class="article-results__buttons article-results__action--send"
-                @click="updateLastPosition">
-                {{ $t("confirm") }}
-              </button>
-            </form>
+            <AdminDashboard @updateLastPositionData="updateLastPositionData"/>
           </template>
           <ul class="article-results__list">
             <li
               v-for="article in articles"
               :key="article.dropboxId"
               class="article-results__item">
-              <article-card
+              <ArticleCard
                 :article="article"
                 :admin-mode="adminMode"/>
             </li>
@@ -96,17 +36,17 @@
 </template>
 
 <script>
-  import ArticleCard from './ArticleCard.vue'
-  import articlesApi from '../api/articles'
-  import positionsApi from '../api/positions'
-  import syncApi from '../api/sync'
-  import notificationsService from '../services/notifications'
-  import articlesSorter from '../services/articlesSorter'
+  import ArticleCard from './ArticleCard.vue';
+  import AdminDashboard from './admin-dashboard/AdminDashboard';
+  import articlesApi from '../api/articles';
+  import positionsApi from '../api/positions';
+  import articlesSorter from '../services/articlesSorter';
 
   export default {
     name: 'ArticleList',
     components: {
-      'article-card': ArticleCard,
+      AdminDashboard,
+      ArticleCard,
     },
     props: {
       adminMode: { type: Boolean, default: () => false },
@@ -114,10 +54,7 @@
     data() {
       return {
         articles: [],
-        isClickedSync: false,
         lastPosition: '',
-        place: null,
-        time: null,
       }
     },
     computed: {
@@ -138,6 +75,9 @@
       },
 
       updateLastPositionData({ place, time }) {
+        console.log('article list update l p d')
+        console.log(place)
+        console.log(time)
         this.lastPosition = `${place}, ${time}`
       },
 
@@ -145,123 +85,20 @@
         positionsApi.fetchLast()
           .then(this.updateLastPositionData)
       },
-
-      submit(e) {
-        e.preventDefault()
-        this.updateLastPosition()
-      },
-
-      updateLastPosition() {
-        const position = {
-          place: this.place,
-          time: this.time,
-        }
-        positionsApi.add(position)
-          .then(this.updateLastPositionData)
-      },
-
-      disableButton() {
-        this.isClickedSync = true
-      },
-
-      enableButton() {
-        this.isClickedSync = false
-      },
-
-      synchronise() {
-        this.trackEvent()
-        this.disableButton()
-        notificationsService.information(this, this.$t('syncLaunched'))
-        syncApi.launch()
-          .then(() => {
-            notificationsService.removeInformation(this)
-            notificationsService.success(this, this.$t('syncDone'))
-          })
-          .then(() => this.goToHome())
-          .catch(err => {
-            notificationsService.removeInformation(this)
-            notificationsService.error(this, `${this.$t('syncError')} ${err}`)
-          })
-      },
-
-      deleteAll() {
-        this.disableButton()
-        notificationsService.information(this, this.$t('syncLaunched'))
-        articlesApi.deleteAll()
-          .then(() => {
-            notificationsService.removeInformation(this)
-            notificationsService.success(this, this.$t('syncDone'))
-          })
-          .then(() => this.goToHome())
-          .catch(err => {
-            notificationsService.removeInformation(this)
-            notificationsService.error(this, `${this.$t('syncError')} ${err}`)
-          })
-      },
-
-      deleteAndSyncAll() {
-        this.disableButton()
-        notificationsService.information(this, this.$t('syncLaunched'))
-        articlesApi.deleteAndSyncAll()
-          .then(() => {
-            notificationsService.removeInformation(this)
-            notificationsService.success(this, this.$t('syncDone'))
-          })
-          .then(() => this.goToHome())
-          .catch(err => {
-            notificationsService.removeInformation(this)
-            notificationsService.error(this, `${this.$t('syncError')} ${err}`)
-          })
-      },
-      goToSubscriptions() {
-        this.$router.push('/subscriptions')
-      },
-      trackEvent() {
-        this.$ga.event({
-          eventCategory: 'Article List',
-          eventAction: 'synchronise',
-          eventLabel: 'All articles have been synchronised',
-        })
-      },
-
-      goToHome() {
-        this.enableButton()
-        this.$router.push('/')
-      },
     },
 
     i18n: {
       messages: {
         fr: {
-          getNewArticles: 'Récupérer les nouveaux articles',
-          deleteAllArticles: 'Supprimer tous les articles',
-          deleteAndSyncAllArticles: 'Supprimer & synchro tous les articles',
-          getSubscribers: 'Récupérer les abonnés de Recontact.me',
           fixWebsite: 'Réparer le site',
           theArticlesOfTheTrip: 'Blog de voyage de Pierre Trollé et Benoît Lefebvre après un tour du monde et d’autres aventures',
-          syncLaunched: 'La synchronisation est lancée ! Patientez quelques secondes...',
-          syncDone: 'La synchronisation s’est effectuée sans problème&nbsp!',
-          syncError: 'Erreur : Problème durant la synchronisation :',
-          place: 'Position :',
-          time: 'Date :',
-          confirm: 'Envoyer',
           lastPosition: 'Dernière position :',
           subtitle: 'Pierre en voyage',
           lastKnownPosition: 'Dernière position connue :',
         },
         en: {
-          getNewArticles: 'Synchronise the new articles',
-          deleteAllArticles: 'Delete all articles',
-          deleteAndSyncAllArticles: 'Delete and synchronise all articles',
-          getSubscribers: 'Get the subscribers',
           fixWebsite: 'Fix the website',
           theArticlesOfTheTrip: 'Travel blog of Pierre Trollé and Benoît Lefebvre after a world trip and other adventures',
-          syncLaunched: 'The synchronisation is launched! Please wait...',
-          syncDone: 'The synchronisation succeeds!',
-          syncError: 'Error during the synchronisation:',
-          place: 'Position:',
-          time: 'Date:',
-          confirm: 'Confirm',
           lastPosition: 'Last position:',
           subtitle: 'Traveling Pierre',
           lastKnownPosition: 'Last known position:',
@@ -320,47 +157,6 @@
       flex-direction: row;
       flex-wrap: wrap;
     }
-  }
-
-  .article-results__input {
-    background: #ffffff;
-    border: 1px solid cadetblue;
-    padding: 15px 10px;
-    border-radius: 4px;
-    width: 230px;
-    margin-bottom: 10px;
-    font-weight: 700;
-  }
-
-  .article-results__buttons {
-    text-transform: uppercase;
-    color: #f76252;
-    background: #ffffff;
-    border: 1px solid #d14800;
-    cursor: pointer;
-    padding: 15px 30px;
-    border-radius: 4px;
-    width: 230px;
-    margin-bottom: 10px;
-    font-weight: 700;
-  }
-
-  .article-results__sync_hidden {
-    color: #f7b5a9;
-  }
-
-  .article-results__buttons:hover {
-    background: #d14800;
-    color: #ffffff;
-  }
-
-  .article-results__buttons:disabled,
-  .article__footer button:active {
-
-    background: #BDBDBD;
-    border-color: #616161;
-    color: #FAFAFA;
-    cursor: auto;
   }
 
 </style>
