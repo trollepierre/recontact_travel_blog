d73ea2ada6074f8dc4abe8b2e50b612dcc97a243
Pierre TROLLE <pierre.trolle@manomano.com>
2019-05-28 22:14:36 +0200

    chore(api): use apiService instead of axios

--- client/src/api/positions.js
+++ client/src/api/positions.js
@@ -1,19 +1,12 @@
-import axios from 'axios'
-import env from '../env/env'
+import apiService from '../services/api-service'
 
 export default {
 
   fetchLast() {
-    const url = `${env('API_URL')}api/positions/last`
-    const options = { headers: { 'Content-Type': 'application/json' } }
-    return axios.get(url, options)
-      .then(response => response.data)
+    return apiService.get('positions/last')
   },
 
   add(position) {
-    const url = `${env('API_URL')}api/positions`
-    const options = { headers: { 'Content-Type': 'application/json' } }
-    return axios.post(url, position, options)
-      .then(response => response.data)
+    return apiService.post('positions', position)
   },
 }
--- client/src/api/positions.spec.js
+++ client/src/api/positions.spec.js
@@ -1,100 +1,41 @@
-import axios from 'axios'
 import positionsApi from './positions'
-import env from '../env/env'
+import apiService from '../services/api-service'
 
 describe('Unit | API | positions api', () => {
   describe('#fetchLast', () => {
     let data
 
     beforeEach(() => {
-      data = {
-        lastPosition: 'Cancun, Mexico, le 5 mars 2018',
-      }
-      const stubbedResponse = {
-        status: 200,
-        data,
-      }
-      axios.get = jest.fn()
-      axios.get.mockResolvedValue(stubbedResponse)
+      data = { lastPosition: 'Cancun, Mexico, le 5 mars 2018' }
+      apiService.get = jest.fn()
+      apiService.get.mockResolvedValue(data)
     })
 
-    it('should fetchLast API with the good params', () => {
-      const expectedUrl = `${env('API_URL')}api/positions/last`
-      const expectedOptions = { headers: { 'Content-Type': 'application/json' } }
+    it('should get good url and return data', async () => {
+      const result = await positionsApi.fetchLast()
 
-      const promise = positionsApi.fetchLast()
-
-      return promise.then(() => {
-        expect(axios.get).toHaveBeenCalledWith(expectedUrl, expectedOptions)
-      })
-    })
-
-    it('should return the response', () => {
-      const promise = positionsApi.fetchLast()
-
-      return promise.then(returnedChapters => {
-        expect(returnedChapters).toEqual(data)
-      })
-    })
-
-    it('should return a rejected promise when an error is thrown', done => {
-      axios.get.mockRejectedValue(new Error('some error'))
-
-      const promise = positionsApi.fetchLast()
-
-      promise.catch(error => {
-        expect(error.message).toEqual('some error')
-        done()
-      })
+      expect(apiService.get).toHaveBeenCalledWith('positions/last')
+      expect(result).toEqual(data)
     })
   })
 
   describe('#add', () => {
     let data
+    const position = {
+      lastPosition: 'Cancun, Mexico, le 5 mars 2018',
+    }
 
     beforeEach(() => {
-      data = {
-        lastPosition: 'Cancun, Mexico, le 5 mars 2018',
-      }
-      const stubbedResponse = {
-        status: 200,
-        data,
-      }
-      axios.post = jest.fn()
-      axios.post.mockResolvedValue(stubbedResponse)
+      data = { lastPosition: 'Cancun, Mexico, le 5 mars 2018' }
+      apiService.post = jest.fn()
+      apiService.post.mockResolvedValue(data)
     })
 
-    it('should set last position thanks to API with the good params', () => {
-      const position = {
-        lastPosition: 'Cancun, Mexico, le 5 mars 2018',
-      }
-      const expectedUrl = `${env('API_URL')}api/positions`
-      const expectedOptions = { headers: { 'Content-Type': 'application/json' } }
+    it('should post to good API with the good params and return result', async () => {
+      const result = await positionsApi.add(position)
 
-      const promise = positionsApi.add(position)
-
-      return promise.then(() => {
-        expect(axios.post).toHaveBeenCalledWith(expectedUrl, position, expectedOptions)
-      })
-    })
-
-    it('should return the response', () => {
-      const promise = positionsApi.add()
-
-      return promise.then(returnedChapters => {
-        expect(returnedChapters).toEqual(data)
-      })
-    })
-
-    it('should return a rejected promise when an error is thrown', done => {
-      axios.post.mockRejectedValue(new Error('some error'))
-
-      const promise = positionsApi.add()
-
-      promise.catch(error => {
-        expect(error.message).toEqual('some error')
-        done()
-      })
+      expect(apiService.post).toHaveBeenCalledWith('positions', position)
+      expect(result).toEqual(data)
     })
   })
 })
