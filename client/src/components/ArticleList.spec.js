8d451caa02c45ab314ce8bbc86be9bf6694e9e16
Pierre TROLLE <pierre.trolle@manomano.com>
2019-05-31 16:55:53 +0200

    chore(test): fix skipped tests

--- client/src/components/ArticleList.spec.js
+++ client/src/components/ArticleList.spec.js
@@ -4,9 +4,7 @@ import VueAnalytics from 'vue-analytics'
 
 import articlesApi from '../api/articles'
 import ArticleList from './ArticleList.vue'
-import syncApi from '../api/sync'
 import positionsApi from '../api/positions'
-import notificationsService from '../services/notifications'
 import articlesSorter from '../services/articlesSorter'
 
 describe('Component | ArticleList.vue', () => {
@@ -24,14 +22,19 @@ describe('Component | ArticleList.vue', () => {
     enTitle: 'The title',
   })
 
-  const fetchArticles = [article('92'), article('12')]
+  const fetchedArticles = [article('92'), article('12')]
   const sortedArticles = [article('12'), article('92')]
+  const lastPosition = { place: 'Mexico', time: '1er mai 2019' }
 
   beforeEach(() => {
+    console.warn = jest.fn()
+
     articlesSorter.sortByDropboxId = jest.fn()
     articlesSorter.sortByDropboxId.mockReturnValue(sortedArticles)
+    positionsApi.fetchLast = jest.fn()
+    positionsApi.fetchLast.mockResolvedValue(lastPosition)
     articlesApi.fetchAll = jest.fn()
-    articlesApi.fetchAll.mockResolvedValue(fetchArticles)
+    articlesApi.fetchAll.mockResolvedValue(fetchedArticles)
     localVue = createLocalVue()
     localVue.use(VueI18n)
     localVue.use(VueRouter)
@@ -55,9 +58,16 @@ describe('Component | ArticleList.vue', () => {
       expect(articlesApi.fetchAll).toHaveBeenCalledWith()
     })
 
-    // TODO kill red warn
     it('should save articles from api in data articles', () => {
       expect(wrapper.vm.articles).toEqual(sortedArticles)
+    })
+
+    it('should call positions api to fetch last position', () => {
+      expect(positionsApi.fetchLast).toHaveBeenCalledWith()
+    })
+
+    it('should save last position from api in data last position', () => {
+      expect(wrapper.vm.lastPosition).toEqual('Mexico, 1er mai 2019')
     })
   })
 
@@ -70,139 +80,27 @@ describe('Component | ArticleList.vue', () => {
       expect(wrapper.vm.title).toEqual('fixWebsite')
     })
 
-    it('should return "Les articles du voyage" when site is in adminMode', () => {
-      const propsData = { adminMode: false }
-
-      wrapper = shallowMount(ArticleList, { localVue, router, propsData })
+    it('should return "Les articles du voyage" by default', () => {
+      wrapper = shallowMount(ArticleList, { localVue, router })
 
       expect(wrapper.vm.title).toEqual('theArticlesOfTheTrip')
     })
   })
 
   describe('methods', () => {
-    describe.skip('#updateLastPosition', () => {
-      beforeEach(() => {
-        positionsApi.add = jest.fn()
-      })
-
-      it('should call positionsApi to add default position', () => {
-        positionsApi.add.mockResolvedValue({})
+    describe('#updateLastPositionData', () => {
+      it('should concat place and time', () => {
         const position = {
-          place: null,
-          time: null,
+          place: 'place',
+          time: 'time',
         }
 
-        wrapper.vm.updateLastPosition()
+        wrapper.vm.updateLastPositionData(position)
 
-        expect(positionsApi.add).toHaveBeenCalledWith(position)
+        expect(wrapper.vm.lastPosition).toEqual('place, time')
       })
-
-      it('should updateLastPositionData with api answer', async () => {
-        positionsApi.add.mockResolvedValue({
-          place: 'London',
-          time: '6 May 2018',
-        })
-
-        await wrapper.vm.updateLastPosition()
-
-        expect(wrapper.vm.lastPosition).toEqual('London, 6 May 2018')
-      })
-    })
-
-    describe.skip('#synchronise', () => {
-      beforeEach(() => {
-        syncApi.launch = jest.fn()
-      })
-
-      it('should display success toast notification before synchronisation calls', () => {
-        notificationsService.information = jest.fn()
-        notificationsService.information.mockResolvedValue({})
-        syncApi.launch.mockResolvedValue({})
-
-        wrapper.vm.synchronise()
-
-        const message = 'syncLaunched'
-        expect(notificationsService.information).toHaveBeenCalledWith(expect.anything(), message)
-      })
-
-      it('should call syncApi', () => {
-        syncApi.launch.mockResolvedValue({})
-
-        wrapper.vm.synchronise()
-
-        expect(syncApi.launch).toHaveBeenCalledWith()
-      })
-
-      it('should display success toast notification when synchronisation succeeds', async () => {
-        notificationsService.success = jest.fn()
-        notificationsService.success.mockResolvedValue({})
-        notificationsService.removeInformation = jest.fn()
-        notificationsService.removeInformation.mockResolvedValue({})
-        syncApi.launch.mockResolvedValue({})
-
-        await wrapper.vm.synchronise()
-
-        expect(notificationsService.removeInformation).toHaveBeenCalledWith(expect.anything())
-        const message = 'syncDone'
-        expect(notificationsService.success).toHaveBeenCalledWith(expect.anything(), message)
-      })
-
-      // xit('should redirect to /', () => {
-      //   router.push.mockResolvedValue({})
-      //   syncApi.launch.mockResolvedValue({})
-      //
-      //   wrapper.vm.synchronise()
-      //
-      //   return Vue.nextTick().then(() => {
-      //     expect(wrapper.vm.$router.push).toHaveBeenCalledWith('/')
-      //   })
-      // })
-      //
-      // xit('should display error toast notification when synchronisation fails', () => {
-      //   notificationsService.error = jest.fn()
-      //   notificationsService.error.mockResolvedValue({})
-      //   syncApi.launch.mockRejectedValue('Expected error')
-      //
-      //   wrapper.vm.synchronise()
-      //
-      //   return Vue.nextTick().then(() => {
-      //     expect(notificationsService.removeInformation).toHaveBeenCalledWith(expect.anything())
-      //     const message = 'syncError Error: Expected error'
-      //     expect(notificationsService.error).toHaveBeenCalledWith(expect.anything(), message)
-      //   })
-      // })
     })
   })
-
-  // describe('clicking on button "Synchronise"', () => {
-  //   let syncButton
-  //
-  //   beforeEach(() => {
-  //     notificationsService.information = jest.fn()
-  //     syncApi.launch.mockResolvedValue({})
-  //     syncButton = wrapper.findAll('button.article-results__buttons').at(1)
-  //   })
-  //
-  //   // xit('should disable sync button', () => {
-  //   //   notificationsService.information.mockRejectedValue()
-  //   //   syncButton.trigger('click')
-  //   //
-  //   //   return Vue.nextTick().then(() => {
-  //   //     expect(syncButton.disabled).toEqual(true)
-  //   //   })
-  //   // })
-  //   //
-  //   // xit('should call synchronise api', () => {
-  //   //   notificationsService.information.mockResolvedValue({})
-  //   //   sinon.stub(component, 'synchronise').mockResolvedValue({})
-  //   //
-  //   //   syncButton.trigger('click')
-  //   //
-  //   //   return Vue.nextTick().then(() => {
-  //   //     expect(wrapper.vm.synchronise).toHaveBeenCalled
-  //   //   })
-  //   // })
-  // })
 
   describe('locales', () => {
     const languages = Object.keys(ArticleList.i18n.messages)
