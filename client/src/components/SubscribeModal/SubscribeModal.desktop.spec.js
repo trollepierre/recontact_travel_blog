a3d9ee55bd422a0983f00c4a7bedeb643911b42c
Pierre TROLLE <pierre.trolle@manomano.com>
2019-06-16 17:41:00 +0200

    feat(modal): subscribe modal width depends on mobile size

---
+++ client/src/components/SubscribeModal/SubscribeModal.desktop.spec.js
@@ -0,0 +1,32 @@
+import VueRouter from 'vue-router'
+import VueI18n from 'vue-i18n'
+import VueAnalytics from 'vue-analytics'
+import SubscribeModal from './SubscribeModal.vue'
+
+describe('Component | SubscribeModal.vue', () => {
+  let wrapper
+  const email = 'pierre@recontact.me'
+  let localVue
+
+  beforeEach(() => {
+    console.warn = jest.fn()
+    localVue = createLocalVue()
+    localVue.use(VueI18n)
+    localVue.use(VueRouter)
+    localVue.use(VueAnalytics, { id: '12' })
+    wrapper = shallowMount(SubscribeModal, {
+      localVue,
+      data() {
+        return {
+          email,
+        }
+      },
+    })
+  })
+
+  describe('template', () => {
+    it('should match snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
