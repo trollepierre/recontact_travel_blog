04813f11b7d986b37c77b944ebc72702a661976e
Pierre TROLLE <pierre.trolle@manomano.com>
2019-06-16 16:56:27 +0200

    chore(structure): move app-header inside its own folder

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-  import AppHeader from './components/AppHeader.vue'
+  import AppHeader from './components/AppHeader/AppHeader.vue'
   import FeedbackModal from './components/FeedbackModal.vue'
   import SubscribeModal from './components/SubscribeModal.vue'
 
