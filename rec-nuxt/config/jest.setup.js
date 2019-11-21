2baf2d613b2de91bcd276ed7b8fb557de37338ed
Pierre TROLLÉ <pierre.trolle@manomano.com>
2019-11-21 13:42:34 +0100

    feat(test): make test script running

---
+++ rec-nuxt/config/jest.setup.js
@@ -0,0 +1,43 @@
+import { createLocalVue, shallowMount, mount, RouterLinkStub } from '@vue/test-utils'
+
+// Export vue-test-utils methods globally
+global.RouterLinkStub = RouterLinkStub
+global.createLocalVue = createLocalVue
+global.shallowMount = shallowMount
+global.mount = mount
+
+expect.extend({
+  toHaveBeenDispatchedToStoreWith(received, argument) {
+    expect(received).toHaveBeenCalledTimes(1)
+    expect(received).toBeCalledWith(expect.anything(), argument, undefined)
+    return { pass: true }
+  },
+  toHaveBeenCalledOnceWith(received, ...args) {
+    expect(received).toHaveBeenCalledTimes(1)
+    expect(received).toHaveBeenCalledWith(...args)
+    return { pass: true }
+  },
+  toHaveBeenNotifiedOnceWith(received, ...args) {
+    expect(received).toHaveBeenCalledTimes(1)
+    expect(received).toHaveBeenCalledWith(expect.anything(), ...args)
+    return { pass: true }
+  },
+  toEmit(received, eventName, data) {
+    if (data) {
+      expect(received.emitted()[eventName][0]).toEqual([data])
+    } else {
+      expect(received.emitted()[eventName][0]).toEqual([])
+    }
+    return { pass: true }
+  },
+  toBeQueriedWith(received, query, payload, callNumber = 0) {
+    expect(received.query.mock.calls[callNumber][0].query).toEqual(query)
+    expect(received.query.mock.calls[callNumber][0].variables).toEqual(payload)
+    return { pass: true }
+  },
+  toBeMutatedWith(received, query, payload, callNumber = 0) {
+    expect(received.mutate.mock.calls[callNumber][0].mutation).toEqual(query)
+    expect(received.mutate.mock.calls[callNumber][0].variables).toEqual(payload)
+    return { pass: true }
+  },
+})
