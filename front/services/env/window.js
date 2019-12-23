3ca04e4e64c61581f7ff0b2ae77845793b2cde91
Pierre TROLLÉ <pierre.trolle@manomano.com>
2019-12-24 00:52:29 +0100

    fix(ci): deploy preprod

--- front/services/env/window.js
+++ front/services/env/window.js
@@ -1,8 +1,8 @@
 const get = key => {
-  return { API_URL: 'https://recontact.herokuapp.com/' }
+   // return { API_URL: 'https://recontact.herokuapp.com/' }
   // return {API_URL: 'https://recontact-api.herokuapp.com/'}
-  // return {API_URL: 'https://recontact-branch.herokuapp.com/'}
-  // return {API_URL: 'http://localhost:3001/'}
+  return { API_URL: 'https://recontact-test.herokuapp.com/' }
+  // return {API_URL: 'http://localhost:3333/'}
   // return process[key]
   //
   // const window = {} // TODO : remove it
