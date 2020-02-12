3c2e3e108aaeed8a3c8e77c94c1066298ad8ce8e
Pierre TROLLÉ <pierre.trolle@manomano.com>
2020-02-12 18:46:50 +0100

    fix(SSR): window was not defined during generation

--- front/services/location/location.js
+++ front/services/location/location.js
@@ -1,5 +1,16 @@
-const isCecile = () => window.location.host === 'cecile.recontact.me'
-const isWww = () => window.location.host === 'www.recontact.me'
+const isCecile = () => {
+  if (process.client) {
+    return window.location.host === 'cecile.recontact.me'
+  }
+  return false
+}
+
+const isWww = () => {
+  if (process.client) {
+    return window.location.host === 'www.recontact.me'
+  }
+  return false
+}
 
 export {
   isCecile,
