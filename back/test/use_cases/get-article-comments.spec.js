b9bdd10213c9c4e791de008b99cde273822df636
Pierre Trollé <trolle.pierre@gmail.com>
2020-02-25 09:16:34 +0100

    fix(test): add lolex

--- back/test/use_cases/get-article-comments.spec.js
+++ back/test/use_cases/get-article-comments.spec.js
@@ -1,8 +1,20 @@
+import lolex from 'lolex'
 import { expect, sinon } from '../test-helper'
 import GetArticleComments from '../../src/use_cases/get-article-comments'
 import CommentRepository from '../../src/domain/repositories/comment-repository'
 
 describe('Unit | GetArticleComments | getArticleComments', () => {
+  const now = '2018-07-21T10:00:00'
+  let clock
+
+  beforeEach(() => {
+    clock = lolex.install({ now: new Date(now).valueOf() })
+  })
+
+  afterEach(() => {
+    clock.uninstall()
+  })
+
   const comments = [
     { id: 1, text: 'comment-1', createdAt: '2020-01-15T12:26:45.887Z' },
     { id: 2, text: 'comment-2', createdAt: '2020-01-15T12:26:45.887Z' },
