ea6d0903e629fc1e562ea9fdbdef0e36679407ff
Pierre Trollé <trolle.pierre@gmail.com>
2020-03-03 22:57:14 +0100

    review(bsr)

---
+++ back/test/domain/utils/date-utils.spec.js
@@ -0,0 +1,52 @@
+import lolex from 'lolex'
+
+import { expect } from '../../test-helper'
+import { formatDateWithLongEndianLongFormat } from '../../../src/domain/utils/date-utils'
+
+describe('Unit | Utils | date-utils', () => {
+  const now = '2018-07-21T10:00:00'
+  let clock
+
+  beforeEach(() => {
+    clock = lolex.install({ now: new Date(now).valueOf() })
+  })
+
+  afterEach(() => {
+    clock.uninstall()
+  })
+
+  describe('#formatDateWithLongEndianLongFormat', () => {
+    it('should return a string with date format dd/MM/yyyy', () => {
+      // Given
+      const date = new Date('2018-01-22')
+
+      // When
+      const formattedDate = formatDateWithLongEndianLongFormat(date)
+
+      // Then
+      expect(formattedDate).to.equal('22/01/2018 01:00')
+    })
+
+    it('should return empty string with date null', () => {
+      // Given
+      const date = null
+
+      // When
+      const formattedDate = formatDateWithLongEndianLongFormat(date)
+
+      // Then
+      expect(formattedDate).to.equal('')
+    })
+
+    it('should handle comments from CommentCard', () => {
+      // Given
+      const date = new Date('2020-01-15T12:26:45.887Z')
+      // When
+      const formattedDate = formatDateWithLongEndianLongFormat(date)
+
+      // Then
+      expect(formattedDate).to.equal('15/01/2020 13:26')
+    })
+  })
+})
+
--- front/components/common/CommentCard.spec.js
+++ front/components/common/CommentCard.spec.js
@@ -3,7 +3,7 @@ import CommentCard from './CommentCard.vue'
 describe('Component | CommentCard.vue', () => {
   let localVue
   let wrapper
-  const propsData = { comment: { text: 'comment', author: 'toto', createdAt: '13/01/2019 22:12' } }
+  const propsData = { comment: { text: 'comment', author: 'toto', createdAt: '13/01/2019 21:12' } }
 
   beforeEach(() => {
     localVue = createLocalVue()
