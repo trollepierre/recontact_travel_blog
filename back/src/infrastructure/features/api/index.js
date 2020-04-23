d4984492bd1e02f2ee2bf9db38644be6e0549ce9
Pierre TROLLÉ <pierre.trolle@manomano.com>
2020-04-23 17:01:03 +0200

    review(bsr): app import global of route

---
+++ back/src/infrastructure/features/api/index.js
@@ -0,0 +1,21 @@
+import articles from './articles'
+import admin from './admin'
+import comments from './comments'
+import sync from './sync'
+import status from './status'
+import subscriptions from './subscriptions'
+import feedbacks from './feedbacks'
+import positions from './positions'
+import optimisation from './optimisation'
+
+export {
+  articles,
+  admin,
+  comments,
+  sync,
+  status,
+  subscriptions,
+  feedbacks,
+  positions,
+  optimisation,
+}
