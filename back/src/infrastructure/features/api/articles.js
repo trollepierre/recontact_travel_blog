18935341e9c35fc7c767ef0eefa9ecff4b573535
Pierre Recontact <commit@recontact.me>
2020-05-21 22:29:11 +0200

    fix(express): use res.status().json

--- back/src/infrastructure/features/api/articles.js
+++ back/src/infrastructure/features/api/articles.js
@@ -8,20 +8,20 @@ import AddComment from '../../../use_cases/add-comment'
 const router = express.Router()
 
 router.get('/', (req, res) => GetAllArticles.getAllArticles()
-  .then(articles => res.json(articles)))
+  .then(articles => res.status(200).json(articles)))
 
 router.get('/:id', (req, res) => GetArticle.getArticle(req.params.id)
-  .then(article => res.json(article)))
+  .then(article => res.status(200).json(article)))
 
 router.get('/:id/photos', (req, res) => GetArticlePhotos.getArticlePhotos(req.params.id)
-  .then(photos => res.json(photos)))
+  .then(photos => res.status(200).json(photos)))
 
 router.get('/:id/comments', (req, res) => GetArticleComments.getArticleComments(req.params.id)
-  .then(comments => res.json(comments)))
+  .then(comments => res.status(200).json(comments)))
 
 router.post('/:id/comments', (req, res) => {
   AddComment.addComment(req.body, req.params.id)
-    .then(comment => res.json(comment))
+    .then(comment => res.status(200).json(comment))
     .catch(error => res.status(400).send(error))
 })
 
--- back/src/infrastructure/features/api/comments.js
+++ back/src/infrastructure/features/api/comments.js
@@ -5,7 +5,7 @@ import DeleteComment from '../../../use_cases/delete-comment'
 const router = express.Router()
 
 router.get('/', (req, res) => GetAllComments.getAllComments()
-  .then(comments => res.json(comments)))
+  .then(comments => res.status(200).json(comments)))
 
 router.delete('/:id', (req, res) => DeleteComment.deleteComment(req.params.id)
   .then(() => res.status(204).send()))
