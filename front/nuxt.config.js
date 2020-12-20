03762886a87ae6035c657dc76da4ab2f960560d9
Pierre Recontact <commit@recontact.me>
2020-12-20 17:18:57 +0100

    fix(meta): charset ut8 added by nuxt/pwa

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -7,7 +7,6 @@ const headEn = {
   },
   title: 'Recontact.Me - Travelling Blog',
   meta: [
-    { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     {
       hid: 'description',
