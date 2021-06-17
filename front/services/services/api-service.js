1edda103fcc0f6268f13a798f27e0926ae318d27
Pierre Recontact <commit@recontact.me>
2021-06-17 23:23:10 +0200

    fix(cache): use cache enhancer

--- front/services/services/api-service.js
+++ front/services/services/api-service.js
@@ -1,13 +1,17 @@
 import axios from 'axios'
+import { cacheAdapterEnhancer } from 'axios-extensions'
 import env from '../env/env'
 import logger from './logger-service'
 
-const apiPath = `${env('API_URL')}api/`
+const http = axios.create({
+  baseURL: `${env('API_URL')}api/`,
+  headers: { 'Cache-Control': `public, max-age=${24 * 3600}` },
+  adapter: cacheAdapterEnhancer(axios.defaults.adapter),
+})
 
 const getAll = async path => {
   try {
-    const url = `${apiPath}${path}`
-    const response = await axios.get(url, { json: true })
+    const response = await http.get(path, { json: true })
     return response.data
   } catch (error) {
     logger.error(error.message)
@@ -17,8 +21,7 @@ const getAll = async path => {
 
 const post = async (path, data) => {
   try {
-    const url = `${apiPath}${path}`
-    const response = await axios.post(url, { ...data, json: true })
+    const response = await http.post(path, { ...data, json: true })
     return response.data
   } catch (error) {
     logger.error(error.message)
@@ -28,8 +31,7 @@ const post = async (path, data) => {
 
 const put = async (path, data) => {
   try {
-    const url = `${apiPath}${path}`
-    const response = await axios.patch(url, { ...data, json: true })
+    const response = await http.patch(path, { ...data, json: true })
     return response.data
   } catch (error) {
     logger.error(error.message)
@@ -39,8 +41,7 @@ const put = async (path, data) => {
 
 const deleteById = async path => {
   try {
-    const url = `${apiPath}${path}`
-    const response = await axios.delete(url)
+    const response = await http.delete(path)
     return response.data
   } catch (error) {
     logger.error(error.message)
