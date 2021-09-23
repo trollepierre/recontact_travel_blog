bfd888804480799a3e7118007febb20ca0100ec1
Pierre Recontact <commit@recontact.me>
2021-09-24 00:55:49 +0200

    fix(back-update): article update needs more information for debugging

--- back/src/use_cases/get-articles-error.js
+++ back/src/use_cases/get-articles-error.js
@@ -1,8 +1,24 @@
 import GetArticlesMeta from './get-articles-meta'
 
+const analyseArticleError = article => {
+  // eslint-disable-next-line no-param-reassign
+  article.error = []
+  if (article.brokenImgDropboxId !== 0) {
+    article.error.push('missing images of chapter')
+  }
+  if (article.photosCount === 0) {
+    article.error.push('missing photos gallery')
+  }
+  if (article.chaptersCount === 0) {
+    article.error.push('missing chapters')
+  }
+  return article
+}
+
 function getAll() {
   return GetArticlesMeta.getAll()
-    .filter(article => article.photosCount === 0 || article.chaptersCount === 0)
+    .map(analyseArticleError)
+    .filter(article => article.error.length !== 0)
 }
 
 export default {
