fae5c82e4d75c7a04f827fa988de3fefa60ee33d
p.trolle <p.trolle@qwant.com>
2026-01-23 12:23:37 +0100

    chore(front): remove legacy plugins

--- front/plugins-legacy/notification.js
+++
@@ -1,8 +0,0 @@
-// import Vue from 'vue'
-//
-// import Toastr from 'vue-toastr'
-//
-// Vue.component('vue-toastr', Toastr)
-// require('vue-toastr/dist/vue-toastr.css')
-//
-// // TODO missing position
