d030e3821b7a5b69a47eba393f50947b895c3c77
p.trolle <p.trolle@qwant.com>
2026-01-23 12:30:42 +0100

    fix(front): paragraph link fixed

--- front/components/ChapterCard/paragraph-link-utils.js
+++ front/components/ChapterCard/paragraph-link-utils.js
@@ -3,7 +3,12 @@ const urlTester = link => /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)
 const youtubeEmbedUrlTester = link => urlTester(link) && (/youtu.be/.test(link) || /youtube.com/.test(link)) && /embed/.test(link)
 const youtubePlaylistEmbedUrlTester = link => /videoseries\?list=/.test(link)
 const generateIframeLink = link => youtubePlaylistEmbedUrlTester(link) ? link : `${link}?rel=0&modestbranding=1`
-const generateCleanUrlLink = link => link.includes('https://') ? link : `https://${link}`
+const generateCleanUrlLink = link => {
+  const val = String(link || '').trim()
+  if (/^https?:\/\//i.test(val)) { return val }
+  if (/^\/\//.test(val)) { return `https:${val}` }
+  return `https://${val}`
+}
 
 export {
   youtubeEmbedUrlTester,
